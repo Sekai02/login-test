7797c3bbe086e16f11fb255a96ed5d4868670dcd
laam05 <laam05x@gmail.com>
2025-06-01 16:53:36 -0400

    feat/ adding home to testing

--- djangovue/djangovue/urls.py
+++ djangovue/djangovue/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from auth_module.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('auth_module.urls')),
+    path('',home, name='home'),
 ]
