cfa9e6d410997a190061bbc3feffb55a707ec239
LAAM05 <116733085+LAAM05@users.noreply.github.com>
2025-06-02 00:21:26 -0400

    Revert "Feat/backend routes"

--- djangovue/auth_module/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home,LoginView,RegisterView,ProtectedView
-
-urlpatterns = [
-    path('',home, name='home'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('protected/', ProtectedView.as_view(), name='protected'),
-]
--- djangovue/djangovue/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from auth_module.views import home
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include('auth_module.urls')),
-    path('',home, name='home'),
-]
